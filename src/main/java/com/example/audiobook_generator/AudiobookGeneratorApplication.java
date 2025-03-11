a4873ea88cb18a7e66a08d153804948fa5df2f69
Douglas <159087442+chi-qui@users.noreply.github.com>
2025-03-10 21:06:34 -0700

    testing

---
+++ src/main/java/com/example/audiobook_generator/AudiobookGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.audiobook_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiobookGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiobookGeneratorApplication.class, args);
+	}
+
+}
